7cf288d4c49c5bdc98fe7b5d857823278be65c5b
Fadhailaryaa <92069381+Fadhailaryaa@users.noreply.github.com>
2025-03-07 00:00:27 +0700

    Add files via upload

---
+++ Tugas1.java
@@ -0,0 +1,31 @@
+package tugasweek1;
+
+class Tugas1{
+    public static void main(String[] biodata){
+        String nim;
+        nim = "A11.2021.13623";
+        String nama;
+        nama = "Fadhail Arya Pramadika";
+        String alamat;
+        alamat = "Jl. Jangli Krajan";
+        String kota;
+        kota = "Semarang";
+        String kodepos;
+        kodepos = "50255";
+        String telp;
+        telp = "082134781069";
+        String hp;
+        hp = "082134781069";
+        String email;
+        email = "111202113623@mhs.dinus.ac.id";
+
+        System.out.println("Nim :"+nim);
+        System.out.println("Nama :"+nama);
+        System.out.println("alamat :"+alamat);
+        System.out.println("kota :"+kota);
+        System.out.println("kodepos :"+kodepos);
+        System.out.println("telp :"+telp);
+        System.out.println("hp :"+hp);
+        System.out.println("email :"+email);
+    }
+}
